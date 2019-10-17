c87209247cc17d27ef10756eacf233384a027ec8
Felipe Leite <felipe.leite.nazareth@gmail.com>
2019-10-17 16:23:14 -0300

    Movendo adição de produtos para o Redux Saga.

--- frontend/src/store/modules/cart/sagas.js
+++ frontend/src/store/modules/cart/sagas.js
@@ -1,13 +1,29 @@
-import { call, put, all, takeLatest } from 'redux-saga/effects'
+import { call, put, all, takeLatest, select } from 'redux-saga/effects'
+
+import { formatPrice } from '../../../util/format'
 
 import api from '../../../services/api'
 
-import { addToCartSuccess } from './actions'
+import { addToCartSuccess, updateAmount } from './actions'
 
 function* addToCart({ id }) {
-    const response = yield call(api.get, `/products/${id}`)
+    const product = yield select(state => state.cart.find(p => p.id === id))
 
-    yield put(addToCartSuccess(response.data))
+    if(product) {
+        const amount = product.amount + 1
+        yield put(updateAmount(id, amount))
+    }
+    else {
+        const response = yield call(api.get, `/products/${id}`)
+
+        const data = {
+            ...response.data,
+            amount: 1,
+            formattedPrice: formatPrice(response.data.price)
+        }
+
+        yield put(addToCartSuccess(data))
+    }
 }
 
 export default all([
